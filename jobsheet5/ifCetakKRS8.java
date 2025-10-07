733b3ca4d588fdb66a578370e9b0e2002d684353
dewirahayu-cmd <d3wi.r4hayu@gmail.com>
2025-10-07 21:22:23 +0700

    cetak KRS

---
+++ jobsheet5/ifCetakKRS8.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ifCetakKRS8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("--- Cetak KRS SIAKAD ---");
+        System.out.print("Apakah UKT sudah lunas ? (True/False): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silakan cek KRS dan minta tangan DPA");
+            
+        
+        }
+
+    }
+    
+}
